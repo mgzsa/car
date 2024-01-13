4a10534f4aa152d4f7291c145d73cdb73b2fdb75
mgzsa <156302589+mgzsa@users.noreply.github.com>
2024-01-13 11:31:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -63,18 +63,6 @@ def welcome():
 
 # app.run(host='localhost', port=80)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--port", type=int, default=8080)
-    args = parser.parse_args()
-
-    start_server(welcome, port=args.port)
-
-
-
-
-
-
 
 
 
